85fc760a8d6f14d58f098546fdffff31b2549f95
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2024-03-04 13:00:12 -0500

    refactored the code to make it dry and clean

--- src/App.jsx
+++ src/App.jsx
@@ -25,12 +25,24 @@ function Header() {
   );
 }
 
+/** this code works but it is not so clean 
 function CoreConcept(props) {
   return (
     <li>
       <img src={props.image} alt={props.title} />
       <h3>{props.title}</h3>
       <p>{props.description}</p>
+    </li>
+  );
+}
+*/
+/** here is a cleaner version of the code using object destructuring */
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
