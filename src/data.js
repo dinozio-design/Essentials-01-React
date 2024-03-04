9eb5cdd3036c7999d48f47154ed1ebb0315fd9e4
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2024-03-04 12:43:23 -0500

    Created the data.js file

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import componentImage from "./assets/components.png";
+import propsImage from "./assets/config.png";
+import jsxImage from "./assets/jsx-ui.png";
+import stateImage from "./assets/state-mgmt.png";
+
+export const CORE_CONCEPTS = [
+    {
+        image: componentImage,
+        title: "Components",
+        description: "The core UI building block - compose the user interface by combining multiple components." 
+    },
+    {
+        image: propsImage,
+        title: "Props",
+        description: "Make components configurable (and therefore reusable) by passing input data to them." 
+    },
+    {
+        image: jsxImage,
+        title: "JSX",
+        description: "Return (potentially dynamic) HTML(ish) code to define the actual markup that will be rendered." 
+    },
+    {
+        image: stateImage,
+        title: "State Management",
+        description: "React-managed data which, when changed, causes the component to re-render & the UI to update." 
+    },
+];
