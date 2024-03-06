f43b66f78ebe1194601483dcc31113d76697c757
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2024-03-06 13:31:54 -0500

    outputing the list content dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -19,15 +19,21 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts </h2>
           <ul>
+           {/* Cleaning it up and making it more dynamic with the help of map() function
             <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             {/* here is a better and cleaner way of using the props, ONLY if the key value pairs used in the props are exactly the same and the key value pairs in the data object */}
+            {/*}
             <CoreConcept {...CORE_CONCEPTS[1]} />
             <CoreConcept {...CORE_CONCEPTS[2]} />
             <CoreConcept {...CORE_CONCEPTS[3]} />
+             */}
+             {CORE_CONCEPTS.map((conceptItem)=>(
+             <CoreConcept key={conceptItem.title} {...conceptItem} />
+             ))}
           </ul>
         </section>
         <section id="examples">
